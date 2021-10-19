5e755b6a17f9bfc6dde5e44307045718cb1598cb
monther saleh Abdellatif <monthersaleh366@gmail.com>
2021-10-19 19:58:37 +0300

    start learning NodeJS :)

---
+++ Modules/app.js
@@ -0,0 +1,6 @@
+console.log("hellow");
+const result=require("./result");
+console.log(result);
+console.log(result.sequire(5));
+console.log(result.add(1,2));
+console.log(result.PI);
---
+++ Modules/result.js
@@ -0,0 +1,12 @@
+function add (a,b) {
+    return a+b;
+}
+function sequire (s){
+    return(s*s);
+}
+const PI=22/7;
+module.exports= {
+    add :add ,
+    sequire :sequire,
+    PI :PI
+}
