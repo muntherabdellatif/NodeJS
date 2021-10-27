11fb07a8cc8ac2edda755f1075d8d64f532a7da2
monther saleh Abdellatif <monthersaleh366@gmail.com>
2021-10-27 20:33:58 +0300

    more NodeJS

--- Newslatter-Signup/app.js
+++ Newslatter-Signup/app.js
@@ -9,11 +9,39 @@ app.get ("/",function(req,res) {
     res.sendFile(__dirname + "/sign-up.html")
 })
 app.post("/",function(req,res) {
-    let firstName =req.body.FirstName;
-    let lastName =req.body.lastName;
-    let email=req.body.email;
-    console.log(firstName,lastName,email);
+    const firstName =req.body.FirstName;
+    const lastName =req.body.lastName;
+    const email=req.body.email;
+    // console.log(firstName,lastName,email);
+    const data = {
+        members:[
+            {
+                email_address:email,
+                status:"subscribed",
+                merge_fields:{
+                    FNAME:firstName,
+                    LNAME:lastName
+                }
+            }
+        ]
+    }
+    const ListID = "ccc99a8c0f";
+    const APIKey =" ad9e84668cf21b4c1e91059e6c421ab3-us5";
+    const jasonData = JSON.stringify(data);
+    const url =`https:us5.api.mailchimp.com/3.0/lists/${ListID}`;
+    const options={
+        method:"POST",
+        auth:`Monther:${APIKey}`
+    }
+    const request=https.request(url,options,function(responce){
+        responce.on("data",function (data) {
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jasonData);
+    request.end();
 })
 app.listen(3000, function() {
     console.log("running");
 })
+
